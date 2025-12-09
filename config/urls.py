2552388704211a4541cec576ee9f3b06345d2645
claudio-py <claudio.3777@gmail.com>
2025-12-09 20:04:17 -0300

    panel page

--- config/urls.py
+++ config/urls.py
@@ -22,7 +22,7 @@ from django.conf.urls.static import static
 from django.views.generic import RedirectView
 
 urlpatterns = [
-path("", RedirectView.as_view(pattern_name="estoque:lista", permanent=False)),
+path("", RedirectView.as_view(pattern_name="estoque:panel", permanent=False)),
     path("admin/", admin.site.urls),
     path("estoque/", include("estoque_app.urls")),
     path("users/", include("users.urls")),
